7cdb3f6d5cc82551dabde15f2469af293fe32137
MuTa17122003 <113000517+MuTa17122003@users.noreply.github.com>
2022-09-07 09:19:58 +0530

    Add files via upload

---
+++ for_loops/exponent_function.py
@@ -0,0 +1,12 @@
+def exp(base,pow):
+    ans=1
+    for index in range(pow):
+        ans=ans*base
+    return ans
+
+print(exp(5,2))
+
+#or
+
+print(2**3)#2 raised to power 3
+
---
+++ for_loops/itteration.py
@@ -0,0 +1,5 @@
+for index in range(5):
+    if(index==0):
+        print("First itteration")
+    else :
+        print("Not first itteration")
